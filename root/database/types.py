a8d95e0799dfd5768a5a1895dc1a1b9e40564986
Peter <>
2023-03-13 00:05:44 +0100

    here goes all the game info stuff, current char race nums dont match original ones

---
+++ root/database/types.py
@@ -0,0 +1,12 @@
+"""
+    Enum stuff
+"""
+class CharacterRace:
+    WARRIOR_MALE        = 0
+    WARRIOR_FEMALE      = 10
+    SURA_MALE           = 1
+    SURA_FEAMLE         = 11
+    ASSASSIN_MALE       = 2
+    ASSASSIN_FEMALE     = 12
+    SHAMAN_MALE         = 3
+    SHAMAN_FEMALE       = 13
