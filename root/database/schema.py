660e231265d04ddec11eb5a5ead83c78d3076b31
Peter <>
2023-03-13 00:40:23 +0100

    renamed tables

--- root/database/schema.py
+++ root/database/schema.py
@@ -7,8 +7,8 @@ import sqlalchemy as sa
 from .extended_base import ExtendedBase
 from .types import CharacterRace
 
-class Accounts(ExtendedBase):
-    __tablename__ = "Accounts"
+class Account(ExtendedBase):
+    __tablename__ = "Account"
     __table_args__ = {}    
     id      = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name    = sa.Column(sa.String(255))
@@ -16,12 +16,12 @@ class Accounts(ExtendedBase):
     def __init__(self, **kwargs):
         self.update(**kwargs)
 
-class Characters(ExtendedBase):
-    __tablename__ = "Characters"
+class Player(ExtendedBase):
+    __tablename__ = "Player"
     __table_args__ = {}
     id      = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name    = sa.Column(sa.String(255))
-    account = sa.Column(sa.Integer, sa.ForeignKey("Accounts.id"))
+    account = sa.Column(sa.Integer, sa.ForeignKey("Account.id"))
     race    = sa.Column(sa.Integer, default=CharacterRace.WARRIOR_MALE)
     job     = sa.Column(sa.Integer, default=0)
     exp     = sa.Column(sa.Integer, default=0)
